48a0485951d207b84cd1e67fd970e4da8f98b3dd
Stephen Touset <stephen@touset.org>
2013-01-18 17:24:06 -0800

    Make encrypt and decrypt actually work

--- ext/cryptography/na_cl/na_cl.h
+++ ext/cryptography/na_cl/na_cl.h
@@ -9,14 +9,18 @@
 #define RB_NACL_CHECK_STRING(str) \
     Check_Type((str), T_STRING)
 
-#define RB_NACL_CHECK_STRING_LEN(str, len) \
-    RB_NACL_CHECK_STRING((str));           \
-    if (RSTRING_LEN((str)) != (len))       \
-        rb_raise(rb_eArgError, #str " must be %d bytes long", (len));
+#define RB_NACL_CHECK_STRING_LEN(str, len)                                \
+    do {                                                                  \
+        RB_NACL_CHECK_STRING((str));                                      \
+        if (RSTRING_LEN((str)) != (len))                                  \
+            rb_raise(rb_eArgError, #str " must be %d bytes long", (len)); \
+    } while (0)
 
-#define RB_NACL_CALLOC(p, len)				\
-    if (!((p) = calloc((len), sizeof((p)))))		\
-        rb_raise(rb_eNoMemError, "NoMemoryError");
+#define RB_NACL_CALLOC(p, len, block) \
+    if (!((p) = calloc((len), sizeof((p))))) {     \
+        block;                                     \
+        rb_raise(rb_eNoMemError, "NoMemoryError"); \
+    }
 
 #define RB_NACL_CFREE(p, len) \
     if ((p)) {                \
--- ext/cryptography/na_cl/na_cl_secret_box.c
+++ ext/cryptography/na_cl/na_cl_secret_box.c
@@ -8,8 +8,9 @@ static VALUE ruby_crypto_secretbox(
 ) {
     VALUE ciphertext = 0;
 
-    unsigned long long padlen  = crypto_secretbox_ZEROBYTES;
-    unsigned long long mlen    = RSTRING_LEN(message) + padlen;
+    unsigned long long mpadlen = crypto_secretbox_ZEROBYTES;
+    unsigned long long cpadlen = crypto_secretbox_BOXZEROBYTES;
+    unsigned long long mlen    = RSTRING_LEN(message) + mpadlen;
     unsigned char      *m      = 0;
     unsigned char      *c      = 0;
     unsigned char      *n      = RSTRING_PTR(nonce);
@@ -19,13 +20,13 @@ static VALUE ruby_crypto_secretbox(
     RB_NACL_CHECK_STRING_LEN(key,   crypto_secretbox_KEYBYTES);
     RB_NACL_CHECK_STRING_LEN(nonce, crypto_secretbox_NONCEBYTES);
 
-    RB_NACL_CALLOC(m, mlen);
-    RB_NACL_CALLOC(c, mlen);
+    RB_NACL_CALLOC(m, mlen, 0);
+    RB_NACL_CALLOC(c, mlen, RB_NACL_CFREE(m, mlen));
 
-    memcpy(m + padlen, RSTRING_PTR(message), RSTRING_LEN(message));
+    memcpy(m + mpadlen, RSTRING_PTR(message), RSTRING_LEN(message));
 
     if (!crypto_secretbox(c, m, mlen, n, k))
-        ciphertext = rb_str_new(c + padlen, RSTRING_LEN(message));
+        ciphertext = rb_str_new(c + cpadlen, mlen - cpadlen);
 
     RB_NACL_CFREE(m, mlen);
     RB_NACL_CFREE(c, mlen);
@@ -36,6 +37,43 @@ static VALUE ruby_crypto_secretbox(
     return ciphertext;
 }
 
+static VALUE ruby_crypto_secretbox_open(
+    VALUE self,
+    VALUE ciphertext,
+    VALUE nonce,
+    VALUE key
+) {
+    VALUE message = 0;
+
+    unsigned long long mpadlen = crypto_secretbox_ZEROBYTES;
+    unsigned long long cpadlen = crypto_secretbox_BOXZEROBYTES;
+    unsigned long long clen    = RSTRING_LEN(ciphertext) + cpadlen;
+    unsigned char      *m      = 0;
+    unsigned char      *c      = 0;
+    unsigned char      *n      = RSTRING_PTR(nonce);
+    unsigned char      *k      = RSTRING_PTR(key);
+
+    RB_NACL_CHECK_STRING(ciphertext);
+    RB_NACL_CHECK_STRING_LEN(key,   crypto_secretbox_KEYBYTES);
+    RB_NACL_CHECK_STRING_LEN(nonce, crypto_secretbox_NONCEBYTES);
+
+    RB_NACL_CALLOC(m, clen, 0);
+    RB_NACL_CALLOC(c, clen, RB_NACL_CFREE(m, clen));
+
+    memcpy(c + cpadlen, RSTRING_PTR(ciphertext), RSTRING_LEN(ciphertext));
+
+    if (!crypto_secretbox_open(m, c, clen, n, k))
+        message = rb_str_new(m + mpadlen, clen - mpadlen);
+
+    RB_NACL_CFREE(m, clen);
+    RB_NACL_CFREE(c, clen);
+
+    if (!message)
+        rb_raise(cNaClError, "crypto_secretbox_open call failed");
+
+    return message;
+}
+
 void Init_na_cl_secret_box(VALUE mNaCl) {
     VALUE mSecretBox = rb_define_module_under(mNaCl, "SecretBox");
 
@@ -43,6 +81,6 @@ void Init_na_cl_secret_box(VALUE mNaCl) {
     rb_define_const(mSecretBox, "NONCE_LEN", INT2FIX(crypto_secretbox_NONCEBYTES));
     rb_define_const(mSecretBox, "KEY_LEN",   INT2FIX(crypto_secretbox_KEYBYTES));
 
-    rb_define_module_function(mSecretBox, "secretbox",      ruby_crypto_secretbox, 3);
-    // rb_define_module_function(mSecretBox, "secretbox_open", ruby__secretbox_open,  3);
+    rb_define_module_function(mSecretBox, "secretbox",      ruby_crypto_secretbox,      3);
+    rb_define_module_function(mSecretBox, "secretbox_open", ruby_crypto_secretbox_open, 3);
 }
